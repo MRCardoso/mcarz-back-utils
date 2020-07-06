17bccd77d398bdb59f49cd38411753c81620af3d
marlonrcardoso <marlonrcardoso@yahoo.com.br>
2020-07-06 16:26:33 -0300

    centralize isolated method in services file

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,12 @@
-import {prepare, responser} from './handlers/Error'
-import { createFilenameHash, is400, is401, is403, is404 } from './handlers/Services'
-import Mail from './handlers/Mail'
-import server from './handlers/Server'
+import { prepare, responser } from './handlers/Error'
+import { createFilenameHash, server, sendMail, is400, is401, is403, is404 } from './handlers/Services'
 import Modelus from './entities/Model'
 import Validatorus from './entities/Validator'
 
 export {
     prepare as prepareError,
     responser as prepareResponse,
-    Mail as sendMail,
+    sendMail,
     createFilenameHash, 
     is400, 
     is401, 
